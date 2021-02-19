be2a97f8d8676335d8a17ff28c25873db6343c99
Robzlegz <robzlegz@gmail.com>
2021-02-19 20:17:49 +0200

    starting to set up redux

---
+++ src/features/appSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState: {
+    roomId: null,
+  },
+  reducers: {
+    increment: state => {
+      state.value += 1;
+    },
+    decrement: state => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = appSlice.actions;
+
+export const selectCount = state => state.app.value;
+
+export default appSlice.reducer;
